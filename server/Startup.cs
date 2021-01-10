0b0475e7204f1b923fb051d0244d0cd727274d61
polatengin <enpolat@microsoft.com>
2021-01-10 09:45:01 +0300

    cors policy added to services

--- server/Startup.cs
+++ server/Startup.cs
@@ -16,5 +16,16 @@ namespace server
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors(options =>
+      {
+        options.AddPolicy("dev", builder =>
+        {
+          builder.AllowAnyHeader();
+          builder.AllowAnyMethod();
+          builder.AllowAnyOrigin();
+        });
+      });
+    }
+
   }
     }
