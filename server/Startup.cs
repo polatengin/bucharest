bcbbb1436355ebc6b12ff2a245be012d20b9103e
polatengin <enpolat@microsoft.com>
2021-01-10 10:17:25 +0300

    sample location data for 8 cities

--- server/Startup.cs
+++ server/Startup.cs
@@ -34,5 +34,15 @@ namespace server
       });
     }
 
+    private CityInformation[] CITY_LIST = {
+      new CityInformation() { CityName = "Istanbul", Latitude = 41.015137M, Longitude = 28.979530M },
+      new CityInformation() { CityName = "Ankara", Latitude = 39.925533M, Longitude = 32.866287M },
+      new CityInformation() { CityName = "Izmir", Latitude = 38.423733M, Longitude = 27.142826M },
+      new CityInformation() { CityName = "Adana", Latitude = 37.000000M, Longitude = 35.321335M },
+      new CityInformation() { CityName = "Gaziantep", Latitude = 37.066666M, Longitude = 37.383331M },
+      new CityInformation() { CityName = "Diyarbakir", Latitude = 37.910000M, Longitude = 40.240002M },
+      new CityInformation() { CityName = "Izmit", Latitude = 40.766666M, Longitude = 29.916668M },
+      new CityInformation() { CityName = "Tekirdag", Latitude = 40.977779M, Longitude = 27.515278M },
+    };
   }
     }
